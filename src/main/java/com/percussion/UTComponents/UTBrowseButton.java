18c363437dc8f56153fc09fd5946783e78b3e479
Nate Chadwick <nate.chadwick@gmail.com>
2020-10-08 15:16:09 -0400

    initial commit

---
+++ src/main/java/com/percussion/UTComponents/UTBrowseButton.java
@@ -0,0 +1,38 @@
+/*[ UTBrowseButton.java ]******************************************************
+ *
+ * COPYRIGHT (c) 1999 - 2003 by Percussion Software, Inc., Stoneham, MA USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Percussion.
+ *
+ ******************************************************************************/
+package com.percussion.UTComponents;
+
+import com.percussion.guitools.BitmapManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Creates a small, square button that has 3 dots that is typically used to
+ * bring up some sort of browser dialog. The button attempts to maintain a
+ * fixed size.
+ */
+public class UTBrowseButton extends JButton
+{
+   /**
+    * The standard ctor for the browse button object.
+    */
+   public UTBrowseButton()
+   {
+      ImageIcon icon = BitmapManager.getBitmapManager(this.getClass()).getImage(
+         "images/optional.gif" );
+      setIcon( icon );
+      Dimension d = new Dimension( IUTConstants.FIXED_HEIGHT,
+         IUTConstants.FIXED_HEIGHT );
+      setSize( d );
+      setMaximumSize( d );
+      setMinimumSize( d );
+      setPreferredSize( d );
+      setAlignmentY( CENTER_ALIGNMENT);
+   }
+}
