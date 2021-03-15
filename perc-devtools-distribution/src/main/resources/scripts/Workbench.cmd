695188f9d65f542015715a45a80ae7c9b27564dc
Vineet <vineetpeeyuse@gmail.com>
2021-03-15 18:41:29 +0530

    CMS-7498 : "agentlib" debug line in workbench jvm config

--- perc-devtools-distribution/src/main/resources/scripts/Workbench.cmd
+++ perc-devtools-distribution/src/main/resources/scripts/Workbench.cmd
@@ -1,4 +1,34 @@
 @echo off
 echo Java Version:
 java -version
+IF "%1"=="" (
+  ECHO Invalid argument: %1
+  ECHO.
+  ECHO Usage:  %~n0  command
+  ECHO.
+  ECHO Where:  command may be prod, dev
+  ECHO Where:  prod is without debug port enabled suitable for production and dev is with debug port enabled suitable for developers
+  ECHO "Prod command example: Workbench.cmd prod"
+  ECHO "Dev command example: Workbench.cmd dev <port_number>"
+  GOTO:EOF
+)
+IF "%1"=="dev" (
+	if "%2"=="" (
+		ECHO please provide valid debug port number
+		goto end
+	)
+)
+if "%1"=="prod" (goto PROD) else (goto DEV)
+goto end
+
+:PROD
+echo Starting without debug port enabled.
 start workbench\eclipse.exe
+goto end
+
+:DEV
+echo Starting with debug port enabled.
+start workbench\eclipse.exe --launcher.stickyVmargs -vmargs -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=%2
+goto end
+
+:end
--- perc-devtools-distribution/src/main/resources/scripts/Workbench.sh
+++ perc-devtools-distribution/src/main/resources/scripts/Workbench.sh
@@ -13,12 +13,49 @@ case "${unameOut}" in
 esac
 echo ${machine}
 
+if ["$1"==""]; then
+	usage
+fi
+
+function usage() {
+  echo "Usage: $0 {prod | dev}"
+  echo "Where:  command may be prod, dev"
+  echo "Where:  prod is without debug port enabled suitable for production and dev is with debug port enabled suitable for developers"
+  echo "Provide Valid <port_number> if using dev mode"
+  echo "Prod command example: Workbench.cmd prod"
+  echo "Dev command example: Workbench.cmd dev <port_number>"
+  exit 1
+}
+
+
+if ["$1"=="dev"]; then
+	devUsage
+fi
+
+function devUsage(){
+	if ["$2"==""]; then
+		checkPort
+	fi
+}
+
+function checkPort() {
+	echo "Please provide valid debug port number"
+	exit 1
+}
+
 if [[ ${machine} -eq "Mac" ]]
 then
   chmod +x ./workbench/Eclipse.app/Contents/MacOS/eclipse
-  ./workbench/Eclipse.app/Contents/MacOS/eclipse
+  if ["$1"=="prod"]; then
+	./workbench/Eclipse.app/Contents/MacOS/eclipse
+  else
+	./workbench/Eclipse.app/Contents/MacOS/eclipse --launcher.stickyVmargs -vmargs -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$2
+  fi
 else
   chmod +x ./workbench/eclipse
-  ./workbench/eclipse
+  if ["$1"=="prod"]; then
+	./workbench/eclipse
+  else
+	./workbench/eclipse --launcher.stickyVmargs -vmargs -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$2
+  fi
 fi
-
