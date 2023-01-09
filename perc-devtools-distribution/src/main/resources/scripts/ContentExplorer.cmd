37fe2d008d8ae6e1e1d33dd9405051879cc38a04
Nate Chadwick <nate.chadwick@gmail.com>
2023-01-09 13:18:48 -0500

    Remove content explorer as it was relocated back to the main CMS project.

--- perc-devtools-distribution/src/main/resources/scripts/ContentExplorer.cmd
+++ perc-devtools-distribution/src/main/resources/scripts/ContentExplorer.cmd
@@ -1,7 +1,7 @@
 @echo off
-echo "NOTE:  Content Explorer requires an Oracle 1.8 JRE - OpenJDK 8 does not include JavaFX"
+echo "NOTE:  Content Explorer requires an Oracle 1.8 JRE - or an OpenJDK JRE 1.8 with an openjfx overlay"
 echo "Using Java version:"
 java -version
 call DevOptions.cmd
 echo Using JAVA_OPTS=%JAVA_OPTS%
-java %JAVA_OPTS% -jar lib/perc-content-explorer-@project.version@.jar
+java %JAVA_OPTS% -jar lib/DesktopContentExplorer-@project.version@.jar
